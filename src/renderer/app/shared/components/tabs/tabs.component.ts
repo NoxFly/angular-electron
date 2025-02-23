0f66ba86174d2ad5185aabdbc913f289153ff08a
NoxFly <dorian.thivolle@gmail.com>
2025-02-23 22:17:40 +0100

    préservation de l'état entre chaque composants d'un même pivot lorsqu'on navigue entre eux

--- src/renderer/app/shared/components/tabs/tabs.component.ts
+++ src/renderer/app/shared/components/tabs/tabs.component.ts
@@ -1,5 +1,5 @@
 import { NgFor } from '@angular/common';
-import { AfterViewInit, ApplicationRef, ChangeDetectorRef, Component, ContentChildren, createComponent, ElementRef, HostBinding, Input, OnInit, QueryList, Renderer2, ViewChild } from '@angular/core';
+import { AfterViewInit, ApplicationRef, ChangeDetectorRef, Component, ComponentRef, ContentChildren, createComponent, ElementRef, HostBinding, Input, OnInit, QueryList, Renderer2, Type, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TabComponent } from './tab/tab.component';
 
@@ -31,43 +31,20 @@ export class TabsComponent implements OnInit, AfterViewInit {
 
     protected activeTabIndex: number = -1;
 
-    protected selectTab(index: number): void {
-        const newActiveTab = this.tabs.get(index);
-        const oldActiveTab = this.tabs.get(this.activeTabIndex);
-        const vec = Math.sign(index- this.activeTabIndex);
+    private readonly instances = new Map<number, { ref: ComponentRef<any>; container: HTMLElement }>();
 
-        if(!newActiveTab || newActiveTab.disabled || index === this.activeTabIndex) {
+
+    protected selectTab(index: number): void {
+        const oldIndex = this.activeTabIndex;
+        const newActiveTab = this.tabs.get(index);
+        const vec = Math.sign(index - oldIndex);
+
+        if(!newActiveTab || newActiveTab.disabled || index === oldIndex) {
             return;
         }
 
-        this.activeTabIndex = index;
-
-        if(oldActiveTab) {
-            const oldElement = this.content.nativeElement.querySelector(`[data-tab-id="${oldActiveTab.getId()}"]:not(.old)`);
-            console.log(oldActiveTab, oldElement);
-            oldElement?.classList.add('old');
-            oldElement?.classList.add(vec < 0 ? 'right-to-left' : 'left-to-right');
-            oldElement?.setAttribute('tabindex', '-1');
-            oldElement?.addEventListener('animationend', () => {
-                console.log('removing', oldElement);
-                oldElement?.remove();
-            }, { once: true });
-        }
-
-        const newComponent = createComponent(newActiveTab.component, { environmentInjector: this.appRef.injector });
-        const newElement = newComponent.location.nativeElement as HTMLElement;
-
-        const newElementWrapper = this.renderer.createElement('div');
-        
-        this.renderer.addClass(newElementWrapper, 'tab-content');
-        this.renderer.addClass(newElementWrapper, vec < 0 ? 'left-to-right' : 'right-to-left');
-
-        this.renderer.setAttribute(newElementWrapper, 'tabindex', '0');
-        this.renderer.setAttribute(newElementWrapper, 'data-tab-id', newActiveTab.getId());
-        this.renderer.appendChild(newElementWrapper, newElement);
-
-        this.renderer.appendChild(this.content.nativeElement, newElementWrapper);
-        this.appRef.attachView(newComponent.hostView);
+        this.leaveTab(oldIndex, vec);
+        this.enterTab(index, vec);
 
         this.cdr.detectChanges();
     }
@@ -77,6 +54,75 @@ export class TabsComponent implements OnInit, AfterViewInit {
         private readonly cdr: ChangeDetectorRef,
         private readonly renderer: Renderer2,
     ) {}
+
+
+    /**
+     * 
+     */
+    private leaveTab(index: number, vec: number): void {
+        const oldActiveTab = this.tabs.get(index);
+
+        if(!oldActiveTab || !this.instances.has(index)) {
+            return;
+        }
+
+        const old = this.instances.get(index)!;
+        const oldElement = old.container;
+
+        this.renderer.addClass(oldElement, 'old');
+        this.renderer.setAttribute(oldElement, 'data-vec', vec < 0 ? 'right-to-left' : 'left-to-right');
+        this.renderer.setAttribute(oldElement, 'tabindex', '-1');
+
+        oldElement?.addEventListener('animationend', () => {
+            this.renderer.removeClass(oldElement, 'old');
+            this.appRef.detachView(old.ref.hostView);
+            oldElement?.remove();
+        }, { once: true });
+    }
+
+    /**
+     * 
+     */
+    private enterTab(index: number, vec: number): void {
+        const newActiveTab = this.tabs.get(index)!;
+
+        let newComponent: ComponentRef<any>;
+        let newElementWrapper: HTMLElement;
+
+        // déjà visité auparavant (sur la même page)
+        if(this.instances.has(index)) {
+            const niew = this.instances.get(index)!;
+            newComponent = niew.ref;
+            newElementWrapper = niew.container;
+        }
+        // création d'une nouvelle instance car 1ère fois visite de cet onglet
+        else {
+            newComponent = createComponent(newActiveTab.component, { environmentInjector: this.appRef.injector });
+            newElementWrapper = this.renderer.createElement('div');
+
+            this.renderer.addClass(newElementWrapper, 'tab-content');
+            this.renderer.setAttribute(newElementWrapper, 'data-tab-id', newActiveTab.getId());
+            this.instances.set(index, { ref: newComponent, container: newElementWrapper });
+        }
+
+        const newElement = newComponent.location.nativeElement as HTMLElement;
+        this.renderer.appendChild(newElementWrapper, newElement);
+        
+        this.renderer.setAttribute(newElementWrapper, 'data-vec', vec < 0 ? 'left-to-right' : 'right-to-left');
+        this.renderer.setAttribute(newElementWrapper, 'tabindex', '0');
+
+        this.renderer.addClass(newElementWrapper, 'new');
+
+        newElementWrapper.addEventListener('animationend', () => {
+            this.renderer.removeClass(newElementWrapper, 'new');
+        }, { once: true });
+
+        this.renderer.appendChild(this.content.nativeElement, newElementWrapper);
+        this.appRef.attachView(newComponent.hostView);
+
+        this.activeTabIndex = index;
+    }
+
 
     public ngAfterViewInit(): void {
         const defaultTabIndex = this.tabs.toArray().findIndex((tab) => tab.default);
