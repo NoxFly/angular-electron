1d13c9fc9434cde8bb146d2f3e1cd4c0f09747a2
Yassine Debbih <yassine.debbih@capvision.fr>
2025-03-14 01:16:37 +0100

    Add Electron Forge configuration and update package scripts

---
+++ software/forge.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    makers: [
+      {
+        name: '@electron-forge/maker-squirrel',
+        config: {
+          authors: 'Dorian et Yassine (un peu)',
+          description: 'An example Angular/Electron app'
+        }
+      }
+    ]
+  };
+  
